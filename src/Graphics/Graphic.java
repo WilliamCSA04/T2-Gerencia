3ac3952c118b1696dc4b14f0106dec61096cbdda
williamcsa04 <williamcaetano14@gmail.com>
2016-10-15 16:03:44 -0300

    Copied code to graphic class for tests

--- src/Graphics/Graphic.java
+++ src/Graphics/Graphic.java
@@ -17,6 +17,112 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 public class Graphic {
     
- 
+ ChartPanel chartPanel = null;
+    JFrame frame = null;
+
+    public void criaGrafico(String titulo, String tituloGrafico, ArrayList<Point> lista) {
+        JFreeChart xylineChart = ChartFactory.createXYLineChart(
+                tituloGrafico,
+                "Tempo",
+                "Valor",
+                createDataset(lista),
+                PlotOrientation.VERTICAL,
+                true, true, false);
+
+        if (chartPanel == null) {
+            chartPanel = new ChartPanel(xylineChart);
+        } else {
+            chartPanel.setChart(xylineChart);
+        }
+
+        chartPanel.setPreferredSize(new java.awt.Dimension(560, 367));
+        final XYPlot plot = xylineChart.getXYPlot();
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.BLUE);
+        renderer.setSeriesPaint(1, Color.GREEN);
+        renderer.setSeriesStroke(0, new BasicStroke(4.0f));
+        renderer.setSeriesStroke(1, new BasicStroke(4.0f));
+        plot.setRenderer(renderer);
+
+        if (frame == null) {
+            frame = new JFrame(titulo);
+            frame.setTitle(tituloGrafico);
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            frame.setLayout(new BorderLayout(0, 5));
+            frame.add(chartPanel, BorderLayout.CENTER);
+        } else {
+            frame.add(chartPanel, BorderLayout.CENTER);
+        }
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private XYDataset createDataset(ArrayList<Point> lista) {
+        final XYSeries in = new XYSeries("In");
+        final XYSeries out = new XYSeries("Out");
+
+        for (int i = 0; i < lista.size(); i++) {
+            in.add(lista.get(i).getInX(), lista.get(i).getInY());
+            out.add(lista.get(i).getOutX(), lista.get(i).getOutY());
+        }
+
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(in);
+        dataset.addSeries(out);
+        return dataset;
+    }
+
+    public void criaGrafico2(String titulo, String tituloGrafico, ArrayList<Point> lista) {
+        JFreeChart xylineChart = ChartFactory.createXYLineChart(
+                tituloGrafico,
+                "Tempo",
+                "Valor",
+                createDataset2(lista),
+                PlotOrientation.VERTICAL,
+                true, true, false);
+
+        if (chartPanel == null) {
+            chartPanel = new ChartPanel(xylineChart);
+        } else {
+            chartPanel.setChart(xylineChart);
+        }
+
+        chartPanel.setPreferredSize(new java.awt.Dimension(560, 367));
+        final XYPlot plot = xylineChart.getXYPlot();
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.BLUE);
+        renderer.setSeriesPaint(1, Color.GREEN);
+        renderer.setSeriesStroke(0, new BasicStroke(4.0f));
+        renderer.setSeriesStroke(1, new BasicStroke(4.0f));
+        plot.setRenderer(renderer);
+
+        if (frame == null) {
+            frame = new JFrame(titulo);
+            frame.setTitle(tituloGrafico);
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            frame.setLayout(new BorderLayout(0, 5));
+            frame.add(chartPanel, BorderLayout.CENTER);
+        } else {
+            frame.add(chartPanel, BorderLayout.CENTER);
+        }
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private XYDataset createDataset2(ArrayList<Point> lista) {
+        final XYSeries in = new XYSeries("Utilização");
+
+        for (int i = 0; i < lista.size(); i++) {
+            in.add(lista.get(i).getX(), lista.get(i).getY());
+        }
+
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(in);
+        return dataset;
+    }
     
 }
